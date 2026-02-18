95635913e31d0664623d903160062b1a955b9353
Yaman30 <yamankhouzaie9@gmail.com>
2026-02-18 13:33:23 +0100

    Update test_plot.py

--- test_plot.py
+++ test_plot.py
@@ -54,22 +54,7 @@ class Robot:
 
         self.finished = False
 
-#Referens punkter
-waypoints = np.array([
-    [0, 0],
-    [5, 0],
-    [10, 7],
-    [13, 5],
-    [16, 5],
-    [20, 8],
-    [25, -3]
-])
 
-# Robotens
-x = 0.0
-y = 0.0
-theta = 0.0
-speed = 0.2
 
 # Inställningar
 
@@ -102,9 +87,6 @@ robots = [r1, r2]
 
 #LQR
 
-# Kartan
-fig, ax = plt.subplots(figsize=(10, 5))
-ax.set_xlim(-5, 30)
 def get_lqr_gain(v_nom):
     if v_nom < 0.1: v_nom = 0.1
     A = np.array([[1.0, v_nom * Ts], [0.0, 1.0]])
@@ -244,24 +226,10 @@ def update(frame):
 
         artists.extend([gfx_objects[r]['rect'], gfx_objects[r]['trail'], gfx_objects[r]['target']])
 
-# Hindren
-obstacle = patches.Rectangle((8, -2), 4, 4, color='brown', label='Hinder')
-ax.add_patch(obstacle)
 
-# Banan
-'''
-for p in waypoints:
-    ax.plot(p[0], p[1], 'rx')
-'''
-ax.plot(waypoints[:, 0], waypoints[:, 1], 'k--', alpha=0.2, linewidth=5, label='bana')
-ax.plot(waypoints[:, 0], waypoints[:, 1], 'mp', label='Waypoints')
     return artists
 
-# Roboten som är en Rektangel
-robot_rect = patches.Rectangle((0, 0), L, W, color='cyan', alpha=0.5, label='Robot')
-ax.add_patch(robot_rect)
 
-ax.legend()
 ani = FuncAnimation(fig, update, frames=600, interval=30, blit=True)
 plt.legend(loc='upper right')
 plt.show()
